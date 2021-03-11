f8eda07da921e14db825ef7917993e01e26352fd
girafffee <46720998+girafffee@users.noreply.github.com>
2021-03-11 09:16:07 +0200

    Added possibility to redeclare version of JFB to compare by `jfb_version_compare`

--- src/JetFormBuilder/WithJFBInit.php
+++ src/JetFormBuilder/WithJFBInit.php
@@ -6,15 +6,19 @@ namespace JFBCore\JetFormBuilder;
 
 trait WithJFBInit {
 
-	public function jfb_maybe_init() {
+	final public function jfb_maybe_init() {
 		if ( $this->can_init() ) {
 			$this->_on_jbf_init();
 		}
 	}
 
-	public function can_init() {
+	public function jfb_version_compare() {
+		return '1.1.0';
+	}
+
+	final public function can_init() {
 		return ( function_exists( 'jet_form_builder' )
-		         && version_compare( JET_FORM_BUILDER_VERSION, '1.1.0', '>=' ) );
+		         && version_compare( JET_FORM_BUILDER_VERSION, $this->jfb_version_compare(), '>=' ) );
 	}
 
 	abstract public function _on_jbf_init();
