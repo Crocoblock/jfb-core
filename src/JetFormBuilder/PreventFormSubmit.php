e48f66602419583096f0a290bd21f0c0e8a2eddd
girafffee <46720998+girafffee@users.noreply.github.com>
2021-03-12 12:25:52 +0200

    Fixed managing hooks in PreventFormSubmit

--- src/JetFormBuilder/PreventFormSubmit.php
+++ src/JetFormBuilder/PreventFormSubmit.php
@@ -7,27 +7,40 @@ namespace JFBCore\JetFormBuilder;
 abstract class PreventFormSubmit {
 
 	public function __construct() {
-		if ( wp_doing_ajax() ) {
-			add_action(
-				'wp_ajax_' . jet_form_builder()->form_handler->hook_key,
-				array( $this, '_prevent_ajax_submit' ), 0
-			);
-			add_action(
-				'wp_ajax_nopriv_' . jet_form_builder()->form_handler->hook_key,
-				array( $this, '_prevent_ajax_submit' ), 0
-			);
-		}
+		add_action( 'after_setup_theme', array( $this, 'manage_hooks' ) );
 	}
 
-	abstract public function prevent_process_ajax_form( $handler );
+	public function manage_hooks() {
+		if ( ! wp_doing_ajax() ) {
+			return;
+		}
 
-	public function _prevent_ajax_submit() {
 		$handler = jet_form_builder()->form_handler;
 
 		remove_action(
 			'wp_ajax_' . $handler->hook_key,
 			array( $handler, 'process_ajax_form' )
 		);
+
+		remove_action(
+			'wp_ajax_nopriv_' . $handler->hook_key,
+			array( $handler, 'process_ajax_form' )
+		);
+
+		add_action(
+			'wp_ajax_' . $handler->hook_key,
+			array( $this, '_prevent_ajax_submit' ), 0
+		);
+		add_action(
+			'wp_ajax_nopriv_' . $handler->hook_key,
+			array( $this, '_prevent_ajax_submit' ), 0
+		);
+	}
+
+	abstract public function prevent_process_ajax_form( $handler );
+
+	public function _prevent_ajax_submit() {
+		$handler = jet_form_builder()->form_handler;
 
 		$handler->is_ajax = true;
 		$handler->setup_form();
