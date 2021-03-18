3e45d53857e316a2ce880778f64a4eb4e7cdc81e
girafffee <46720998+girafffee@users.noreply.github.com>
2021-03-18 11:39:10 +0200

    Added `set_json_meta` to MetaQuery class

--- src/Common/MetaQuery.php
+++ src/Common/MetaQuery.php
@@ -29,4 +29,20 @@ class MetaQuery {
 		return $meta ? json_decode( $meta, true ) : array();
 	}
 
+	public static function set_json_meta( array $params ) {
+		$params = array_merge( array(
+			'id' => 0,
+			'key' => '',
+			'value' => array()
+		), $params );
+
+		[ 'id' => $post_id, 'key' => $key, 'value' => $value ] = $params;
+
+		if ( ! $post_id ) {
+			$post_id = get_the_ID();
+		}
+
+		return update_post_meta( $post_id, $key, json_encode( $value ) );
+	}
+
 }
