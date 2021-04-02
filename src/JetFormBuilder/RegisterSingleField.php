0efcc59ac49dbcba3d2c2c8d8fd0889f86c62620
girafffee <46720998+girafffee@users.noreply.github.com>
2021-04-02 10:30:33 +0300

    Update RegisterSingleField.php

--- src/JetFormBuilder/RegisterSingleField.php
+++ src/JetFormBuilder/RegisterSingleField.php
@@ -8,6 +8,24 @@ trait RegisterSingleField {
 
 	use EditorAssetsManager;
 
+	/**
+	 * For backward compatibility.
+	 * Now this value is set via block.json
+	 * @return string
+	 */
+	public function get_title() {
+		return '';
+	}
+
+	/**
+	 * For backward compatibility.
+	 * Now this value is set via block.json
+	 * @return array
+	 */
+	public function get_attributes() {
+		return array();
+	}
+
 	public static function register() {
 		if ( ! function_exists( 'jet_form_builder' ) ) {
 			return;
